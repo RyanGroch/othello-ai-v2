ac3b5a5b8f5274c702c567ce6497e4b23287dd84
Thrasymachuss <ryan.w.groch@gmail.com>
2022-11-26 17:55:21 -0600

    Deploy attempt #3

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "othello-ai-v2",
+  base: "/othello-ai-v2/",
 });
